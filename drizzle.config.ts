18af75d720dc74aa88e4884ec27197be4f6bee59
uke_mba <uke_sjtu@sjtu.edu.cn>
2025-10-11 20:45:19 +0800

    refactor(db): update schema path and add index file

--- drizzle.config.ts
+++ drizzle.config.ts
@@ -2,7 +2,7 @@ import { defineConfig } from "drizzle-kit";
 
 export default defineConfig({
   out: "./drizzle",
-  schema: "./src/lib/db/schema.ts",
+  schema: "./src/lib/schema/index.ts",
   dialect: "postgresql",
   dbCredentials: {
     url: process.env.DATABASE_URL!,
