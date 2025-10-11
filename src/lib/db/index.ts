da7d5b6c248c88913d75a9949865f6f27dc3add8
uke_mba <uke_sjtu@sjtu.edu.cn>
2025-10-11 20:37:35 +0800

    feat(db): add drizzle configuration and database connection setup

---
+++ src/lib/db/index.ts
@@ -0,0 +1,8 @@
+import { drizzle } from "drizzle-orm/node-postgres";
+
+export const db = drizzle({
+  connection: {
+    connectionString: process.env.DATABASE_URL,
+    ssl: process.env.NODE_ENV === "production",
+  },
+});
