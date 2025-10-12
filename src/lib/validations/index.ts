cfbbda8a3686be209d8c9beae693bf006abe527e
ukeSJTU <uke_sjtu@sjtu.edu.cn>
2025-10-12 16:27:29 +0800

    feat(validations): add initial validation schemas export structure

---
+++ src/lib/validations/index.ts
@@ -0,0 +1,7 @@
+// Export all validation schemas and utilities from common.ts
+export * from "./example";
+
+// Future module exports can be added here:
+// export * from "./auth";
+// export * from "./user";
+// export * from "./booking";
