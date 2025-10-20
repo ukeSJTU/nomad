71c324e26efe066c7710461dffef6fd529d92262
uke_mba <uke_sjtu@sjtu.edu.cn>
2025-10-20 19:24:14 +0800

    feat(docs): add configuration files for documentation and MDX support

---
+++ source.config.ts
@@ -0,0 +1,47 @@
+import {
+  rehypeCodeDefaultOptions,
+  remarkMdxFiles,
+} from "fumadocs-core/mdx-plugins";
+import {
+  defineConfig,
+  defineDocs,
+  frontmatterSchema,
+  metaSchema,
+} from "fumadocs-mdx/config";
+import { transformerTwoslash } from "fumadocs-twoslash";
+import rehypeKatex from "rehype-katex";
+import remarkMath from "remark-math";
+import { type BundledLanguage, bundledLanguages } from "shiki";
+
+// You can customise Zod schemas for frontmatter and `meta.json` here
+// see https://fumadocs.dev/docs/mdx/collections
+export const docs = defineDocs({
+  docs: {
+    schema: frontmatterSchema,
+    postprocess: {
+      includeProcessedMarkdown: true,
+    },
+  },
+  meta: {
+    schema: metaSchema,
+  },
+});
+
+export default defineConfig({
+  mdxOptions: {
+    // MDX options
+    remarkPlugins: [remarkMath, remarkMdxFiles],
+    rehypePlugins: v => [rehypeKatex, ...v],
+    rehypeCodeOptions: {
+      themes: {
+        light: "github-light",
+        dark: "github-dark",
+      },
+      langs: Object.keys(bundledLanguages) as BundledLanguage[],
+      transformers: [
+        ...(rehypeCodeDefaultOptions.transformers ?? []),
+        transformerTwoslash(),
+      ],
+    },
+  },
+});
