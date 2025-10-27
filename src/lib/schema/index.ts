7ddf6ca613023b0f6f4dc04fc1e57e43ba71f8fb
uke_mba <uke_sjtu@sjtu.edu.cn>
2025-10-27 14:31:41 +0800

    feat(schema): export cities schema in index file

--- src/lib/schema/index.ts
+++ src/lib/schema/index.ts
@@ -1,6 +1,7 @@
 export * from "../../../auth-schema";
 export * from "./airlines";
 export * from "./airports";
+export * from "./cities";
 export * from "./flight-seat-classes";
 export * from "./flights";
 export * from "./passengers";
