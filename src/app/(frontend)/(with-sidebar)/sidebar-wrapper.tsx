9779e216121b96c8487f3e2fcc56dfc6fb0e465c
ukeSJTU <uke_sjtu@sjtu.edu.cn>
2025-11-13 00:30:10 +0800

    feat(layout): update layouts to include header, footer, and toaster components

--- src/app/(frontend)/(with-sidebar)/sidebar-wrapper.tsx
+++ src/app/(frontend)/(with-sidebar)/sidebar-wrapper.tsx
@@ -1,7 +1,8 @@
 import { type ReactNode, Suspense } from "react";
 
-import { MainSidebar } from "@/components/common";
+import { Footer, Header, MainSidebar } from "@/components/common";
 import { SidebarInset, SidebarProvider } from "@/components/ui/sidebar";
+import { Toaster } from "@/components/ui/sonner";
 
 export function SidebarWrapper({ children }: { children: ReactNode }) {
   return (
@@ -9,7 +10,14 @@ export function SidebarWrapper({ children }: { children: ReactNode }) {
       <Suspense fallback={<div>Loading...</div>}>
         <MainSidebar />
       </Suspense>
-      <SidebarInset>{children}</SidebarInset>
+      <SidebarInset>
+        <div className="flex min-h-screen flex-col">
+          <Header />
+          <Toaster />
+          <main className="flex-1">{children}</main>
+          <Footer />
+        </div>
+      </SidebarInset>
     </SidebarProvider>
   );
 }
--- src/app/(frontend)/layout.tsx
+++ src/app/(frontend)/layout.tsx
@@ -2,16 +2,6 @@ import "./globals.css";
 
 import { type ReactNode } from "react";
 
-import { Footer, Header } from "@/components/common";
-import { Toaster } from "@/components/ui/sonner";
-
 export default function FrontendLayout({ children }: { children: ReactNode }) {
-  return (
-    <div className="flex min-h-screen flex-col">
-      <Header />
-      <Toaster />
-      <main className="flex-1">{children}</main>
-      <Footer />
-    </div>
-  );
+  return children;
 }
