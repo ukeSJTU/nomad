4869d7b2ee5350826e358e2f2ffc26e162440535
uke_mba <uke_sjtu@sjtu.edu.cn>
2025-11-16 11:37:24 +0800

    refactor(api): remove passengers API schemas and update exports

--- src/types/api/index.ts
+++ src/types/api/index.ts
@@ -1,9 +1,8 @@
 /**
  * src/types/api/ is the folder for API validation schemas
  *
- * Even we are mostly using Server Actions in this project, APIs are still reserved for external services.
+ * Even though we are mostly using Server Actions in this project, APIs are still reserved for external services.
  */
 
 export * from "./health";
-export * from "./passengers";
 export * from "./response";
