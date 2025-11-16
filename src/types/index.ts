283afabd67b2f7c286e25a2c5f07fb3b5a1771d3
uke_mba <uke_sjtu@sjtu.edu.cn>
2025-11-16 10:57:49 +0800

    refactor(example): remove example validation schemas and tests

--- src/types/index.ts
+++ src/types/index.ts
@@ -1,9 +1,3 @@
 // Export all validation schemas and utilities from example.ts
 export * from "./api";
 export * from "./auth";
-export * from "./example";
-
-// Future module exports can be added here:
-// export * from "./auth";
-// export * from "./user";
-// export * from "./booking";
