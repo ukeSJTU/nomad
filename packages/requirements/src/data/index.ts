cbdc22b80ebd49e1d205d8a7db819cfedd633c49
ukeSJTU <uke_sjtu@sjtu.edu.cn>
2025-12-28 10:19:19 +0800

    docs: convert all flight moduel requirements

--- packages/requirements/src/data/index.ts
+++ packages/requirements/src/data/index.ts
@@ -1,12 +1,12 @@
+import { flightModule } from "./flight-module";
 import type { ModuleDefinition } from "./types";
 import { userModule } from "./user-module";
 
-// 目前只导出用户模块，其他模块待迁移
-// flightModule,  // 待迁移
+// 目前只导出用户，机票模块，其他模块待迁移
 // orderModule,   // 待迁移
 // paymentModule, // 待迁移
 // uiUxModule,    // 待迁移
-export const allModules: ModuleDefinition[] = [userModule];
+export const allModules: ModuleDefinition[] = [userModule, flightModule];
 
-export { userModule };
+export { userModule, flightModule };
 export * from "./types";
