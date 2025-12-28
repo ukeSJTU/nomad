5c8bea4208e95d686c98ac6e8fa1ff0015210645
ukeSJTU <uke_sjtu@sjtu.edu.cn>
2025-12-28 10:38:42 +0800

    docs: convert all ui-ux module requirements

--- packages/requirements/src/data/index.ts
+++ packages/requirements/src/data/index.ts
@@ -2,16 +2,16 @@ import { flightModule } from "./flight-module";
 import { orderModule } from "./order-module";
 import { paymentModule } from "./payment-module";
 import type { ModuleDefinition } from "./types";
+import { uiUxModule } from "./ui-ux-module";
 import { userModule } from "./user-module";
 
-// 目前只导出用户，机票模块，其他模块待迁移
-// uiUxModule,    // 待迁移
 export const allModules: ModuleDefinition[] = [
   userModule,
   flightModule,
   orderModule,
   paymentModule,
+  uiUxModule,
 ];
 
-export { userModule, flightModule, orderModule, paymentModule };
+export { userModule, flightModule, orderModule, paymentModule, uiUxModule };
 export * from "./types";
