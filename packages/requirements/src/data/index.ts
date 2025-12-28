383b6c7ac8a1b416696720ff9b40d56f61809fa2
ukeSJTU <uke_sjtu@sjtu.edu.cn>
2025-12-28 10:26:28 +0800

    docs: convert all payment module requirements

--- packages/requirements/src/data/index.ts
+++ packages/requirements/src/data/index.ts
@@ -1,16 +1,17 @@
 import { flightModule } from "./flight-module";
 import { orderModule } from "./order-module";
+import { paymentModule } from "./payment-module";
 import type { ModuleDefinition } from "./types";
 import { userModule } from "./user-module";
 
 // 目前只导出用户，机票模块，其他模块待迁移
-// paymentModule, // 待迁移
 // uiUxModule,    // 待迁移
 export const allModules: ModuleDefinition[] = [
   userModule,
   flightModule,
   orderModule,
+  paymentModule,
 ];
 
-export { userModule, flightModule, orderModule };
+export { userModule, flightModule, orderModule, paymentModule };
 export * from "./types";
