37e522091726ae48b0368859a29ea4aadc540345
ukeSJTU <uke_sjtu@sjtu.edu.cn>
2025-12-28 10:24:52 +0800

    docs: convert all order module requirements

--- packages/requirements/src/data/index.ts
+++ packages/requirements/src/data/index.ts
@@ -1,12 +1,16 @@
 import { flightModule } from "./flight-module";
+import { orderModule } from "./order-module";
 import type { ModuleDefinition } from "./types";
 import { userModule } from "./user-module";
 
 // 目前只导出用户，机票模块，其他模块待迁移
-// orderModule,   // 待迁移
 // paymentModule, // 待迁移
 // uiUxModule,    // 待迁移
-export const allModules: ModuleDefinition[] = [userModule, flightModule];
+export const allModules: ModuleDefinition[] = [
+  userModule,
+  flightModule,
+  orderModule,
+];
 
-export { userModule, flightModule };
+export { userModule, flightModule, orderModule };
 export * from "./types";
