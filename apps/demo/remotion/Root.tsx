21abc7a7c3ecdd31cc65c024c5e8d1e77e7254c9
ukeSJTU <uke_sjtu@sjtu.edu.cn>
2026-01-03 20:46:49 +0800

    refactor: remove all existing animation code from apps/demo project

--- apps/demo/remotion/Root.tsx
+++ apps/demo/remotion/Root.tsx
@@ -1,79 +1,20 @@
 import { Composition } from "remotion";
+import { VIDEO_FPS, VIDEO_HEIGHT, VIDEO_WIDTH } from "./constants";
 
-import { BusinessShowcase } from "./compositions/BusinessShowcase";
-import {
-  SCENE_DURATIONS,
-  TOTAL_FRAMES,
-  VIDEO_FPS,
-  VIDEO_HEIGHT,
-  VIDEO_WIDTH,
-} from "./constants";
-import { AuthScene } from "./scenes/AuthScene";
-import { AuthSceneComplete } from "./scenes/AuthSceneComplete";
-import { HomeScene } from "./scenes/HomeScene";
-import { IntroScene } from "./scenes/IntroScene";
-import { OrderScene } from "./scenes/OrderScene";
-import { SearchScene } from "./scenes/SearchScene";
+const EmptyScene: React.FC = () => {
+  return (
+    <div style={{ width: "100%", height: "100%", background: "#ffffff00" }} />
+  );
+};
 
 export const RemotionRoot: React.FC = () => {
   return (
     <>
-      {/* 完整业务展示动画 - 主Composition */}
+      {/* Minimal empty Remotion composition for demo */}
       <Composition
-        id="BusinessShowcase"
-        component={BusinessShowcase}
-        durationInFrames={TOTAL_FRAMES}
-        fps={VIDEO_FPS}
-        width={VIDEO_WIDTH}
-        height={VIDEO_HEIGHT}
-        defaultProps={{}}
-      />
-
-      {/* 单独场景 - 用于开发和调试 */}
-      <Composition
-        id="IntroScene"
-        component={IntroScene}
-        durationInFrames={SCENE_DURATIONS.INTRO}
-        fps={VIDEO_FPS}
-        width={VIDEO_WIDTH}
-        height={VIDEO_HEIGHT}
-      />
-      <Composition
-        id="AuthScene"
-        component={AuthScene}
-        durationInFrames={SCENE_DURATIONS.AUTH_SIGNIN}
-        fps={VIDEO_FPS}
-        width={VIDEO_WIDTH}
-        height={VIDEO_HEIGHT}
-      />
-      <Composition
-        id="AuthSceneComplete"
-        component={AuthSceneComplete}
-        durationInFrames={SCENE_DURATIONS.AUTH}
-        fps={VIDEO_FPS}
-        width={VIDEO_WIDTH}
-        height={VIDEO_HEIGHT}
-      />
-      <Composition
-        id="SearchScene"
-        component={SearchScene}
-        durationInFrames={SCENE_DURATIONS.SEARCH}
-        fps={VIDEO_FPS}
-        width={VIDEO_WIDTH}
-        height={VIDEO_HEIGHT}
-      />
-      <Composition
-        id="OrderScene"
-        component={OrderScene}
-        durationInFrames={SCENE_DURATIONS.ORDER}
-        fps={VIDEO_FPS}
-        width={VIDEO_WIDTH}
-        height={VIDEO_HEIGHT}
-      />
-      <Composition
-        id="HomeScene"
-        component={HomeScene}
-        durationInFrames={SCENE_DURATIONS.HOME}
+        id="Empty"
+        component={EmptyScene}
+        durationInFrames={VIDEO_FPS}
         fps={VIDEO_FPS}
         width={VIDEO_WIDTH}
         height={VIDEO_HEIGHT}
